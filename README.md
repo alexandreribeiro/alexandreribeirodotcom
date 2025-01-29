1f88930dbdfadc4798621363a67ba04ff44f9735
Alexandre Ribeiro <aribeiro2@gmail.com>
2025-01-29 20:07:37 +0100

    Add base template rendering
    A template rendering script using jinja2 was created, a few content
    pages were added.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Welcome!
 
 This is Alexandre Ribeiro's personal web-page.
+
+To generate the templates:
+
+```bash
+pip install -r requirements.txt
+python template_renderer.py
+```
---
+++ template_renderer.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+from jinja2 import Environment, FileSystemLoader
+
+root_build_folder = "_build"
+build_folder_list = [root_build_folder, f"{root_build_folder}/world-map", f"{root_build_folder}/about",
+                     f"{root_build_folder}/weather-dashboard"]
+assets_folder = "templates/assets"
+
+if os.path.exists(root_build_folder):
+    print(f"Deleting existing build folder: {root_build_folder}")
+    shutil.rmtree(root_build_folder)
+for folder_to_create in build_folder_list:
+    print(f"Creating new build folder: {folder_to_create}")
+    os.makedirs(folder_to_create)
+print(f"Copying assets to {root_build_folder}")
+shutil.copytree(assets_folder, os.path.join(root_build_folder, os.path.basename(assets_folder)))
+
+jinja_environment = Environment(loader=FileSystemLoader('templates'))
+jinja_environment.globals['ROOT_URL'] = 'https://alexandreribeiro.com'
+
+pages = [
+    {
+        "template": "pages/index.html",
+        "output": "index.html",
+        "data": {}
+    },
+    {
+        "template": "pages/world-map/index.html",
+        "output": "world-map/index.html",
+        "data": {}
+    },
+    {
+        "template": "pages/about/index.html",
+        "output": "about/index.html",
+        "data": {}
+    },
+    {
+        "template": "pages/weather-dashboard/index.html",
+        "output": "weather-dashboard/index.html",
+        "data": {}
+    }
+]
+
+for page in pages:
+    template = jinja_environment.get_template(page["template"])
+    rendered_content = template.render(page["data"])
+    with open("_build/" + page["output"], "w") as f:
+        f.write(rendered_content)
+
+print("Pages have been rendered successfully!")
